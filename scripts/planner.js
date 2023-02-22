521749eb2f6fed1336e840fea119871eedc792ef
Worthy <worthyintech@gmail.com>
2023-02-22 20:13:01 +0000

    Completed the add meal pop up.

--- scripts/planner.js
+++ scripts/planner.js
@@ -9,3 +9,15 @@ setRemaindersBtn.addEventListener("click", () => {
 closeSetRemaindersPopUpBtn.addEventListener("click", () => {
     setRemaindersPopUp.parentElement.classList.add("toggle-popup-display");
 });
+
+const addMealBtn = document.querySelector("#add-meal-btn");
+const closeAddMealPopUpBtn = document.querySelector("#close-add-meal-popup-btn");
+const addMealPopUp = document.querySelector("#add-meal-popup");
+
+addMealBtn.addEventListener("click", () => {
+    addMealPopUp.parentElement.classList.remove("toggle-popup-display");
+});
+
+closeAddMealPopUpBtn.addEventListener("click", () => {
+    addMealPopUp.parentElement.classList.add("toggle-popup-display");
+});
