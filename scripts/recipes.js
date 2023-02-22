6da877bc0ccd35d95bb890ff1c312351b774c50d
Worthy <worthyintech@gmail.com>
2023-02-22 18:56:10 +0000

    The user can now add the ingredient and specify the amount.

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -16,4 +16,27 @@ const ingredientsInputSection = document.querySelector(".recipe-ingredients");
 const addIngredientBtn = document.querySelector("#add-recipe-ingredient-input-btn");
 let numberOfIngredients = 1;
 
-addIngredientBtn.addEventListener();
+addIngredientBtn.addEventListener("click", addIngredientInputField);
+
+function addIngredientInputField() {
+    console.log("You clicked the add ingredient button.")
+    numberOfIngredients++;
+
+    let ingredientGroup = document.createElement("div");
+    ingredientGroup.classList.add("recipe-ingredient-subgroup");
+
+    let ingredientInput = document.createElement("input");
+    ingredientInput.setAttribute("placeholder", "ingredient (e.g. flour)");
+    ingredientInput.setAttribute("name", `recipe-ingredient-${numberOfIngredients}`);
+    ingredientInput.setAttribute("id", `recipe-ingredient-${numberOfIngredients}`);
+
+    let amountInput = document.createElement("input");
+    amountInput.setAttribute("placeholder", "amount (e.g. 250g)");
+    amountInput.setAttribute("name", `recipe-amount-${numberOfIngredients}`);
+    amountInput.setAttribute("id", `recipe-amount-${numberOfIngredients}`);
+
+    ingredientGroup.appendChild(ingredientInput);
+    ingredientGroup.appendChild(amountInput);
+
+    ingredientsInputSection.appendChild(ingredientGroup);
+}
