529106ed88248ff2ed678b43655e434ec460f009
Worthy <worthyintech@gmail.com>
2023-02-23 09:45:54 +0000

    Working on theme functionality.

---
+++ scripts/theme.js
@@ -0,0 +1,7 @@
+const themeBtn = document.querySelector("#theme-btn");
+
+themeBtn.addEventListener("click", toggleTheme);
+
+function toggleTheme() {
+    console.log("You clicked the theme button!");
+}
