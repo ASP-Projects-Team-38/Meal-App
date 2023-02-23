03da4ebe7ca2a682f4bcccae19d59b9902240d57
Worthy <worthyintech@gmail.com>
2023-02-23 10:50:50 +0000

    Created mixin for the themes.

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,7 +1,17 @@
 const themeBtn = document.querySelector("#theme-btn");
+const body = document.querySelector("body");
 
 themeBtn.addEventListener("click", toggleTheme);
 
 function toggleTheme() {
     console.log("You clicked the theme button!");
+
+    if (body.classList.contains("theme--light")) {
+        body.classList.remove("theme--light");
+        body.classList.add("theme--dark");
+    }
+    else {
+        body.classList.remove("theme--dark");
+        body.classList.add("theme--light");
+    }
 }
